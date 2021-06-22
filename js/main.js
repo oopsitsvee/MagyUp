64d94b38d4a69d644dbab21851500153d6b69e72
oopsitsvee <59890905+oopsitsvee@users.noreply.github.com>
2021-06-22 09:23:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,101 +1,23 @@
-// меню
-menu.onclick = function Func(){
-    let x = document.getElementById("TopNav");
 
-    if(x.className === "topnav"){
-        x.className += " close";
-    } 
-    else{
-        x.className = "topnav";
-    }
-};
-
-// породы 
-
-// слайдер
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// функция возвращает cookie с именем name, если есть, если нет, то undefined    
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-// купить щенка
-
-// фильтр 1
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-    let filter, ul, li, a, i;
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
-  };
-// фильтр 2
-  function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-    let filter, ul, li, a, i;
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown2");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
-  };
-// фильтр 3
-  function myFunction3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-    let filter, ul, li, a, i;
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown3");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
-  };
-
-
-  
+let cookiecook = getCookie("cookiecook"),
+cookiewin = document.getElementsByClassName('cookie_notice')[0];    
+// проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
+if (cookiecook != "no") {
+  // показываем    
+  cookiewin.style.display="block"; 
+  // закрываем по клику
+  document.getElementById("cookie_close").addEventListener("click", function(){
+      cookiewin.style.display="none";    
+      // записываем cookie на 1 день, с которой мы не показываем окно
+      let date = new Date;
+      date.setDate(date.getDate() + 1);    
+      document.cookie = "cookiecook=no; path=/; expires=" + date.toUTCString();               
+  });
+}  
